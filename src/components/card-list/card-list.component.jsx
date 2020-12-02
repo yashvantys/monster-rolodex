15fce3b2d2638d59d92418dce305fc93033b47f8
Yashvant Yadav <yashvant.yadav@kitelytech.com>
2020-12-02 08:38:41 +0530

    removed console

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,8 +4,7 @@ import {Card} from '../card/card.component'
 
 import './card-list.styles.css';
 
-export const CardList = props => {
-    console.log(props);
+export const CardList = props => {    
 return (
     <div className="card-list">
         {
